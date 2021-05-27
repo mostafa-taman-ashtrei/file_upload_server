a30ab5daa8b786eb75d5d383b52cce3ea7c1e751
NEO <neo.one.mail@gmail.com>
2021-05-27 04:24:33 +0200

    upload a file

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
-import express from 'express';
 import { config } from 'dotenv';
+import express from 'express';
+import multer from 'multer';
+import fs from 'fs';
 
 (async () => {
     config();
@@ -7,6 +9,29 @@ import { config } from 'dotenv';
     const app = express();
     const port = process.env.PORT || 8080;
 
+    const uploadDir = 'uploads';
+
+    if (!fs.existsSync(uploadDir)) fs.mkdirSync(uploadDir);
+
+    const storageEngine = multer.diskStorage({
+        destination: (_, __, cb) => cb(null, uploadDir),
+        filename: (_, file, cb) => cb(null, `${Date.now()}--${file.originalname}`),
+    });
+
+    const uploadEngine = multer({ storage: storageEngine });
+
     app.get('/', (_, res) => res.json({ msg: 'hello world!' }));
+
+    app.post('/upload', uploadEngine.single('image'), (req, res) => {
+        try {
+            const { file } = req;
+            console.log(file);
+            res.json({ file });
+        } catch (e) {
+            res.json({ e });
+            throw new Error(e);
+        }
+    });
+
     app.listen(port, () => console.log(`Server is running on port ${port} ...`));
 })();
